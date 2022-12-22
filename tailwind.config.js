20deeadf8bef1c80601baa0ff41bf059157d9eaa
Falren <nokuchie@gmail.com>
2022-12-22 23:24:55 +0200

    add dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,19 @@
-module.exports = {
+const colors = require('tailwindcss/colors')
 
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        teal: colors.teal,
+        cyan: colors.cyan
+      }
+    }
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio')
+
+  ],
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
