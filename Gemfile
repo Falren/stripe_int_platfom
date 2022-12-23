9ec3c1f0e7c62113addedccfcde7f0b573ea2ded
Falren <nokuchie@gmail.com>
2022-12-23 19:06:36 +0200

    add dashboard && accounts pages

--- Gemfile
+++ Gemfile
@@ -74,3 +74,5 @@ gem "devise", "~> 4.8"
 gem "sidekiq", "~> 7.0"
 
 gem "stripe", "~> 8.0"
+
+gem "pry", "~> 0.14.1"
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'accounts/index'
   devise_for :users
   root 'static_pages#root'
   post '/webhooks/:source', to: 'webhooks#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :accounts
+  resource :dashboard
   # Defines the root path route ("/")
   # root "articles#index"
 end
